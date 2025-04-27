677ce4958434e0d8b7616b63d034081a514c9625
Mykhailo Orban <mikhail.orban@gmail.com>
2025-04-27 23:44:46 +0300

    Initial commit and root rout

---
+++ test_file.rb
@@ -0,0 +1,7 @@
+sample_hash = {'a' => 1, 'b'=> 2, 'c'=> 3}
+my_details = {'name' => 'mashur', 'favcolor' => 'red'}
+p my_details
+
+ my_details.each do|key, value|
+     puts "The class for key is #{key.class} and the value is #{value.class} "
+end
